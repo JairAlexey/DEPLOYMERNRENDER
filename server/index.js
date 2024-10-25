b1ed844759d789c87af019ae559ec8167ed425b6
JairAlexey <alexeyrueda823@gmail.com>
2024-10-25 17:52:56 -0500

    Preperando para deploy

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import app from "./src/app.js";
+import { PORT } from "./src/config.js";
+import { connectDB } from "./src/db.js";
+
+async function main() {
+  try {
+    await connectDB();
+    app.listen(PORT);
+    console.log(`Listening on port http://localhost:${PORT}`);
+    console.log(`Environment: ${process.env.NODE_ENV}`)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
